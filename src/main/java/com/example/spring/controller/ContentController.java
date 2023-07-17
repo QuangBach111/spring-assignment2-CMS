de283b787f125c19106ae04bfa1656ee3197ddba
nguyenthanhtri00001 <122081749+nguyenthanhtri00001@users.noreply.github.com>
2023-07-17 19:47:20 +0700

    list and delete's updates

--- src/main/java/com/example/spring/controller/ContentController.java
+++ src/main/java/com/example/spring/controller/ContentController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -32,12 +29,16 @@ public class ContentController {
         model.addAttribute("totalPages", page.getTotalPages());
         model.addAttribute("items", page.getTotalElements());
         model.addAttribute("contents", contents);
-        return "content-list";
+        return "content/content-list";
     }
-
-    
-
-
+    @PostMapping("/delete")
+    @DeleteMapping("/delete")
+    public String deleteContent(@RequestParam("selectedContentIds") List<Long> contentIds) {
+        for (Long contentId : contentIds) {
+            contentService.deleteContentById(contentId);
+        }
+        return "redirect:/content/list/1";
+    }
 
 
 

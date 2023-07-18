c0f6dd718fefbf21b611cc787736a8ace92e293f
nguyenthanhtri00001 <122081749+nguyenthanhtri00001@users.noreply.github.com>
2023-07-18 16:20:56 +0700

    Update content-list.js

--- src/main/resources/static/js/content-list.js
+++ src/main/resources/static/js/content-list.js
@@ -1,1 +1,12 @@
+// Add event listener to each delete button
+var deleteForms = document.getElementsByClassName('delete-form');
+Array.from(deleteForms).forEach(function(form) {
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the form from submitting directly
+
+        if (confirm('Are you sure you want to delete this content?')) {
+            this.submit(); // Submit the form
+        }
+    });
+});
 
